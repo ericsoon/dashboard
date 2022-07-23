8248ce77b4e46ebb3f673b03ca946017c1146492
Eric Soon <eric.soonsmk@gmail.com>
2022-07-22 18:19:52 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { BrowerRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icon/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="underline text-3xl">App</h1>
+  );
+}
+
+export default App;
