cd014db0e392286945d9fb36667591b0c5c5c7ec
Eric Soon <eric.soonsmk@gmail.com>
2022-07-28 18:04:00 -0700

    finishing ecommerce section

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
-function SparkLine() {
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+
+function SparkLine({ id, height, width, color, data, type, currentColor }) {
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWeight={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, widht: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: '${x} : data ${y}',
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={{ SparklineTooltip }} />
+    </SparklineComponent>
   );
 }
 
--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 
-function Stacked() {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+
+function Stacked({ width, height }) {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      id="charts"
+      width={width}
+      height={height}
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   );
 }
 
