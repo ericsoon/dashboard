031c04db3b138228dd971e437f685c0d95c29147
Eric Soon <eric.soonsmk@gmail.com>
2022-07-31 22:40:16 -0700

    finished bar component

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Tooltip, Inject, Category, DataLabel, Legend, ColumnSeries } from '@syncfusion/ej2-react-charts';
+
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
 
 function Bar() {
+  const { currentMode } = useStateContext();
   return (
-    <div>Bar</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Bar" />
+      <div className="w-full">
+        <ChartComponent
+          title="Olympic Medal Counts- RIO"
+          id="bar-charts"
+          chartArea={{ border: { width: 0 } }}
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
+          tooltip={{ enable: true }}
+          legendSettings={{ background: 'white' }}
+          background={currentMode === 'Dark' ? '#33373E' : '#FFF'}
+        >
+          <Inject services={[DataLabel, ColumnSeries, Legend, Category, Tooltip]} />
+          <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {barCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   );
 }
 
